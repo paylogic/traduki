bea4c09d9f326fdbb1ba8d0e571521d5ab45146f
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-02-05 09:04:20 +0100

    initial public release

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,10 +1,10 @@
 """Test SQLAlchemy i18n."""
 import pytest
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, UnicodeText
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 import traduki
