fa42ba488ee3d50872cd45e7a51fad7cb398d903
Alessio Bogon <youtux@users.noreply.github.com>
2018-04-11 17:36:55 +0200

    Fix Travis CI (#5)
    * Let pep8 not complain about the configuration
    
    * remove test command from setup.py
    
    * drop support for py26
    
    * Use tox directly in .travis.yml
    
    * add tox to the testing requirements

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 from os.path import abspath, dirname, join
 
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 import sys
-
-
-class ToxTestCommand(TestCommand):
-    """Test command which runs tox under the hood."""
-
-    def finalize_options(self):
-        """Add options to the test runner (tox)."""
-        TestCommand.finalize_options(self)
-        self.test_args = ['--recreate']
-        self.test_suite = True
-
-    def run_tests(self):
-        """Invoke the test runner (tox)."""
-        #import here, cause outside the eggs aren't loaded
-        import detox.main
-        errno = detox.main.main(self.test_args)
-        sys.exit(errno)
 
 long_description = []
 
@@ -41,8 +23,6 @@ setup(
     ],
     packages=find_packages(exclude=['ez_setup', 'tests']),
     dependency_links=[],
-    tests_require=['detox'],
-    cmdclass={'test': ToxTestCommand},
     include_package_data=True,
     keywords='sqlalchemy i18n internationalisation',
 )
