64bf7ce9e34e0927c8fc76448f61c1ecec0e735d
Alessio Bogon <youtux@gmail.com>
2020-02-10 13:28:39 +0100

    Declare python3 compatibility

--- setup.py
+++ setup.py
@@ -25,4 +25,17 @@ setup(
     dependency_links=[],
     include_package_data=True,
     keywords='sqlalchemy i18n internationalisation',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
